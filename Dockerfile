47c2169f3ac62397b7efe37dd1d0c831903cd8f0
BenchiaRazvan <razvanbenchia9716@gmail.com>
2022-03-27 19:57:50 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:16.3
+
+WORKDIR /app
+
+ADD . /app
+
+COPY package*.json .
+
+RUN npm install
+
+COPY . .
+
+CMD node index.js
+
---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+const mysql = require('mysql');
+
+var app = express();
+
+
+app.get("/", (req, res, next) => {
+   
+    res.send("Hello world");
+});
+
+
+
+app.listen(800, () => {
+
+ console.log("Server running on port 800");
+
+});
+
