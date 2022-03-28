76ca42a33d1381741ae15064394e7727e1b44d5b
Fisher121 <58854288+Fisher121@users.noreply.github.com>
2022-03-28 11:36:27 +0300

    sql_test

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ const mysql = require('mysql');
 
 var app = express();
 
+var pool = mysql.createPool({
+    socketPath:'/cloudsql/medicineapi:europe-central2:medicinedbinstance',
+    database: 'questions',
+    user:'username',
+    password:'',
+}) 
 
 app.get("/", (req, res, next) => {
+
+    
+    const query = "SELECT * FROM questions"
+    
+    pool.query(query, [] , (error, result)=>{
+        if(error) throw error
+        if(!result[0]){
+            res.json({status:"not found!"})
+        }
+        else{
+            res.json(result[0])
+        }
+    })
    
-    res.send("Hello Emi test");
 });
 
 
