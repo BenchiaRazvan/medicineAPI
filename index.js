119d141fdbdf019b67a07d51dbe057cd4af0ddf8
Fisher121 <58854288+Fisher121@users.noreply.github.com>
2022-03-28 12:33:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res, next) => {
     const query = "SELECT * FROM questions"
     
     pool.query(query, [] , (error, result)=>{
-        if(error) throw error
+        if(error) res.json({status:"err"})
         if(!result[0]){
             res.json({status:"not found!"})
         }
